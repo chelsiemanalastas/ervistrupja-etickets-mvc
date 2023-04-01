b8a35c9607668e77ecad1cf6cdfd7857c9e0224f
tur-el <manalastaschelsie@gmail.com>
2023-04-01 16:27:30 +0800

    Add project files.

---
+++ ErvisTrupja_eTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ErvisTrupja_eTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
